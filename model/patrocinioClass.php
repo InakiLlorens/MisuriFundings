8db3f841047208b0f35d5971bbb620a633d86496
MarkelGit <47140844+MarkelGit@users.noreply.github.com>
2020-03-05 10:12:36 +0100

    Create patrocinioClass.php

---
+++ model/patrocinioClass.php
@@ -0,0 +1,45 @@
+<?php
+class patrocinioClass {    
+    protected $id;
+    protected $idUsuario;
+    protected $idFunding;
+    protected $idContribucion;
+    
+    function getId() {
+        return $this->id;
+    }
+
+    function getidUsuario() {
+        return $this->idUsuario;
+    }
+
+    function getIdFunding() {
+        return $this->idFunding;
+    }
+
+    function getIdContribucion() {
+        return $this->idContribucion;
+    } 
+
+    function setId($id) {
+        $this->id = $id;
+    }
+
+    function setIdUsuario($idUsuario) {
+        $this->idUsuario = $idUsuario;
+    }
+
+    function setIdFunding($idFunding) {
+        $this->idFunding = $idFunding;
+    }
+
+    function setIdContribucion($idContribucionl) {
+        $this->idContribucion = $idContribucion;
+    }    
+
+    function getObjectVars() {
+        $vars=get_object_vars($this);
+        return  $vars;
+    } 
+}
+?>
