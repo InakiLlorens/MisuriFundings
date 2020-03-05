abf71a025f054c38b48d73121fa1209a7a60e490
MarkelGit <47140844+MarkelGit@users.noreply.github.com>
2020-03-05 11:57:28 +0100

    Create actualizacionModel.php

---
+++ model/actualizacionModel.php
@@ -0,0 +1,50 @@
+<?php
+include_once 'connect_data.php';
+include_once 'actualizacionClass.php';
+
+class actualizacionModel extends actualizacionClass {
+    private $link;
+    private $list=array();
+
+    public function OpenConnect() {
+        $konDat = new connect_data();
+        
+        try {
+            $this->link = new mysqli($konDat->host, $konDat->userbbdd, $konDat->passbbdd, $konDat->ddbbname);        
+        } catch (Exception $e) {
+            echo $e->getMessage();
+        }
+        
+        $this->link->set_charset("utf8"); 
+    }
+
+    public function CloseConnect() {
+        //mysqli_close ($this->link);
+        
+        $this->link->close();
+    }
+
+    public function setList() {
+        $this->OpenConnect();  //Abrir conexión
+        
+        $sql = "CALL spAllActualizaciones()"; //Sentencia SQL
+        
+        $result = $this->link->query($sql); //Se guarda la información solicitada a la bbdd
+        
+        while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+            
+            $actualizacion=new actualizacionModel();
+            
+            $actualizacion->setId($row['id']);
+            $actualizacion->setNombre($row['nombre']);
+            $actualizacion->setDescripcion($row['descripcion']);
+            $actualizacion->setFecha($row['fecha']);
+            $actualizacion->setIdFunding($row['idFunding']);
+            
+            array_push($this->list, $actualizacion);
+        }
+        mysqli_free_result($result);
+        $this->CloseConnect();
+    }
+}
+?>
