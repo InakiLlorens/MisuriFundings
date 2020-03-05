1e36bcec5939e86c985f464c38294dc7ffcb4c95
MarkelGit <47140844+MarkelGit@users.noreply.github.com>
2020-03-05 10:34:58 +0100

    Create votoModel.php

---
+++ model/votoModel.php
@@ -0,0 +1,49 @@
+<?php
+include_once 'connect_data.php';
+include_once 'votoClass.php';
+
+class votoModel extends votoClass {
+    private $link;
+    private $list=array();
+
+    public function OpenConnect() {
+        $konDat = new connect_data();
+        
+        try {
+            $this->link = new mysqli($konDat->host, $konDat->userbbdd, $konDat->passbbdd, $konDat->ddbbname);        
+        } catch (Exception $e) {
+            echo $e->getMessage();
+        }
+        
+        $this->link->set_charset("utf8"); 
+    }
+
+    public function CloseConnect() {
+        //mysqli_close ($this->link);
+        
+        $this->link->close();
+    }
+
+    public function setList() {
+        $this->OpenConnect();  //Abrir conexión
+        
+        $sql = "CALL spAllVotos()"; //Sentencia SQL
+        
+        $result = $this->link->query($sql); //Se guarda la información solicitada a la bbdd
+        
+        while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+            
+            $voto=new votoModel();
+            
+            $voto->setId($row['id']);
+            $voto->setPositivo($row['positivo']);
+            $voto->setIdUsuario($row['idUsuario']);
+	          $voto->setIdFunding($row['idFunding']);
+            
+            array_push($this->list, $voto);
+        }
+        mysqli_free_result($result);
+        $this->CloseConnect();
+    }
+}
+?>
