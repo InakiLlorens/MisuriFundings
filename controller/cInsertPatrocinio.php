4d6dcbd75260d9442b5e2d5d970c52a88dbaf99c
inakiLlorens <inakillorens9@gmail.com>
2020-05-19 22:56:15 +0200

    añadido cosas al controller

--- controller/cInsertPatrocinio.php
+++ controller/cInsertPatrocinio.php
@@ -9,5 +9,13 @@ $patrocinioModel->setIdUsuario($_SESSION["id"]);
 $patrocinioModel->setIdFunding($_SESSION["idFunding"]);
 $patrocinioModel->setIdContribucion($_SESSION["idContribucion"]);
 
+$owner = filter_input(INPUT_POST, "owner");
+$cvv= filter_input(INPUT_POST, "cvv");
+$number= filter_input(INPUT_POST, "number");
+$mes= filter_input(INPUT_POST, "mes");
+$year= filter_input(INPUT_POST, "year");
+
+
+
 $patrocinioModel->insertPatrocinio();
 ?>
