564e220517ac0f914fb621a3f30a550a0d80f9a9
inakiLlorens <inakillorens9@gmail.com>
2020-05-22 23:19:07 +0200

    yo no se por que sigo respirando

--- misuri_fundings.sql
+++ misuri_fundings.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.0.1
+-- version 4.8.5
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 22-05-2020 a las 21:43:16
--- Versión del servidor: 10.4.6-MariaDB
--- Versión de PHP: 7.3.9
+-- Tiempo de generación: 22-05-2020 a las 23:18:48
+-- Versión del servidor: 10.1.40-MariaDB
+-- Versión de PHP: 7.3.5
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -26,6 +26,9 @@ DELIMITER $$
 --
 -- Procedimientos
 --
+CREATE DEFINER=`root`@`localhost` PROCEDURE `spActualizacionesByIdFunding` (IN `inIdFunding` INT)  NO SQL
+SELECT * FROM `actualizacion` WHERE idFunding =inIdFunding$$
+
 CREATE DEFINER=`root`@`localhost` PROCEDURE `spAllActualizaciones` ()  NO SQL
 SELECT * FROM `actualizacion`$$
 
@@ -104,8 +107,11 @@ SELECT SUM(positivo) AS ratioVoto, idFunding, crowdfunding.* FROM voto RIGHT JOI
 CREATE DEFINER=`root`@`localhost` PROCEDURE `spPreguntasByFundingId` (IN `inIdFunding` INT)  NO SQL
 SELECT * FROM `pregunta` WHERE idFunding=inIdFunding$$
 
-CREATE DEFINER=`root`@`localhost` PROCEDURE `spUpdatePregunta` (IN `inIdPregunta` INT(11), IN `inPregunta` VARCHAR(50), IN `inRespuesta` VARCHAR(250), IN `inIdFunding` INT(11))  NO SQL
-UPDATE `pregunta` SET `pregunta`=inPregunta,`respuesta`=inRespuesta,`idFunding`=inIdFunding WHERE pregunta.id=inIdPregunta$$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `spUpdateFundingDineroR` (IN `inId` INT, IN `dinero` INT)  NO SQL
+UPDATE `crowdfunding` SET `dineroR`= dineroR+dinero WHERE id = inId$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `spUpdatePregunta` (IN `inIdPregunta` INT(11), IN `inPregunta` VARCHAR(50), IN `inRespuesta` VARCHAR(250))  NO SQL
+UPDATE `pregunta` SET `pregunta`=inPregunta,`respuesta`=inRespuesta WHERE pregunta.id=inIdPregunta$$
 
 CREATE DEFINER=`root`@`localhost` PROCEDURE `spUpdateVoto` (IN `inPositivo` INT, IN `inIdFunding` INT, IN `inIdUsuario` INT)  NO SQL
 UPDATE `voto` SET `positivo`=inPositivo WHERE idFunding=inIdFunding AND idUsuario = inIdUsuario$$
@@ -128,17 +134,9 @@ CREATE TABLE `actualizacion` (
   `id` int(11) NOT NULL,
   `nombre` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
   `descripcion` varchar(250) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `fecha` datetime NOT NULL DEFAULT current_timestamp(),
+  `fecha` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `idFunding` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Volcado de datos para la tabla `actualizacion`
---
-
-INSERT INTO `actualizacion` (`id`, `nombre`, `descripcion`, `fecha`, `idFunding`) VALUES
-(1, 'ree', 'retert', '0000-00-00 00:00:00', 3),
-(2, 'yrty', 'retert', '2020-05-22 21:34:30', 2);
 
 -- --------------------------------------------------------
 
@@ -152,21 +150,6 @@ CREATE TABLE `comentario` (
   `idUsuario` int(11) NOT NULL,
   `idFunding` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Volcado de datos para la tabla `comentario`
---
-
-INSERT INTO `comentario` (`id`, `comentario`, `idUsuario`, `idFunding`) VALUES
-(1, 'Un gran trabajo Markel.', 2, 4),
-(2, 'adssadsda', 2, 4),
-(3, '			asddassd', 2, 1),
-(4, '			dsadsa', 2, 1),
-(15, 'dsasdds', 2, 3),
-(16, 'dsasdds', 2, 3),
-(17, 'sdads', 2, 3),
-(18, 'sadds', 2, 3),
-(19, '', 2, 3);
 
 -- --------------------------------------------------------
 
@@ -188,11 +171,7 @@ CREATE TABLE `contribucion` (
 --
 
 INSERT INTO `contribucion` (`id`, `nombre`, `precio`, `descripcion`, `recompensa`, `idFunding`) VALUES
-(1, 'Contribución Base', 20, 'La contribución mínima.', 'Un enorme aprobado.', 1),
-(2, 'asdsda', 43, 'dsadsadsa', 'adsdsa', 1),
-(3, 'saddsasd', 1, 'sdadsa', 'sdadsa', 1),
-(5, 'gerg', 3443, 'fddsfds', 'sdfsdf', 22),
-(6, 'uryut', 345, 'gdfg', 'dfgdfg', 22);
+(7, 'Ranger experto', 40, 'Una pequeña suma para una gran saga', '- Copia fisica del juego\n- Libro de arte\n- Aparici', 23);
 
 -- --------------------------------------------------------
 
@@ -207,7 +186,7 @@ CREATE TABLE `crowdfunding` (
   `dineroR` int(11) NOT NULL,
   `dineroO` int(11) NOT NULL,
   `fechaFin` date NOT NULL,
-  `imagen` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `imagen` varchar(360) COLLATE utf8mb4_unicode_ci NOT NULL,
   `idUsuario` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
@@ -216,15 +195,7 @@ CREATE TABLE `crowdfunding` (
 --
 
 INSERT INTO `crowdfunding` (`id`, `nombre`, `descripcion`, `dineroR`, `dineroO`, `fechaFin`, `imagen`, `idUsuario`) VALUES
-(1, 'importante', 'descripcion de algo importante', 200, 400, '2020-04-22', 'annie.jpg', 2),
-(2, 'secundario', 'descripcion de algo importante', 1, 333, '2020-04-23', '', 2),
-(3, 'secundario', 'descripcion de algo secundario', 200, 300, '0000-00-00', '', 2),
-(4, 'terciario', 'descripcion de algo terciario', 200, 300, '2020-04-01', '', 2),
-(5, 'secundario', 'descripcion de algo secundario', 200, 300, '2020-04-30', '', 2),
-(6, 'terciario', 'descripcion de algo terciario', 200, 300, '2020-04-01', '', 2),
-(20, 'zdasd', 'fsdfsd', 0, 2343, '2020-05-30', 'absol.jpg', 2),
-(21, 'rwer', 'sfds', 0, 234, '2020-05-29', 'ashe.jpg', 2),
-(22, 'tert', 'fsdfs', 0, 34534, '2020-05-29', 'annie.jpg', 2);
+(23, 'Pokemon ranger switch', 'Nueva idea para un juego de switch! \n\nTengo muchas ganas de empezar y cuento con vuestro apoyo :)', 0, 1000, '2020-05-30', 'ranger.jpg', 2);
 
 -- --------------------------------------------------------
 
@@ -255,16 +226,6 @@ CREATE TABLE `patrocinio` (
   `titular` varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
---
--- Volcado de datos para la tabla `patrocinio`
---
-
-INSERT INTO `patrocinio` (`id`, `idUsuario`, `idFunding`, `idContribucion`, `CVV`, `numero`, `fechaCad`, `titular`) VALUES
-(1, 2, 1, 1, 0, '0', '0000-00-00', ''),
-(2, 2, 1, 1, 0, '0', '0000-00-00', ''),
-(3, 2, 1, 1, 123, '2147483647', '2020-05-20', 'sdadsadsa'),
-(12, 2, 1, 1, 2343, '4534 3345 4453 3543', '2020-05-23', 'Markel');
-
 -- --------------------------------------------------------
 
 --
@@ -277,17 +238,6 @@ CREATE TABLE `pregunta` (
   `respuesta` varchar(250) COLLATE utf8mb4_unicode_ci NOT NULL,
   `idFunding` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Volcado de datos para la tabla `pregunta`
---
-
-INSERT INTO `pregunta` (`id`, `pregunta`, `respuesta`, `idFunding`) VALUES
-(1, '¿Cuándo estará lista?', 'No lo se.', 4),
-(2, 'asddsa', 'dsadsadsa', 3),
-(3, 'asddsa', 'dsadsadsa', 3),
-(6, 'dsadsaaaaa', 'dasdsa', 1),
-(7, 'rerer', 'rererw', 1);
 
 -- --------------------------------------------------------
 
@@ -309,7 +259,8 @@ CREATE TABLE `usuario` (
 --
 
 INSERT INTO `usuario` (`id`, `nombre`, `apellido`, `usuario`, `contrasena`, `email`) VALUES
-(2, 'sdadsasda', 'sdsadds', 'pruebauser', '123', '');
+(2, 'sdadsasda', 'sdsadds', 'pruebauser', '123', ''),
+(3, 'usuarioNormal', '', 'usuarioNormal', '123', 'usuarionormal@gmail.com');
 
 -- --------------------------------------------------------
 
@@ -329,14 +280,7 @@ CREATE TABLE `voto` (
 --
 
 INSERT INTO `voto` (`id`, `positivo`, `idUsuario`, `idFunding`) VALUES
-(1, 1, 2, 1),
-(2, 1, 2, 1),
-(4, 1, 2, 3),
-(21, 1, 2, 5),
-(22, -1, 2, 2),
-(33, 0, 2, 20),
-(34, 0, 2, 21),
-(35, 0, 2, 22);
+(39, 0, 2, 23);
 
 --
 -- Índices para tablas volcadas
@@ -422,19 +366,19 @@ ALTER TABLE `actualizacion`
 -- AUTO_INCREMENT de la tabla `comentario`
 --
 ALTER TABLE `comentario`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 
 --
 -- AUTO_INCREMENT de la tabla `contribucion`
 --
 ALTER TABLE `contribucion`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT de la tabla `crowdfunding`
 --
 ALTER TABLE `crowdfunding`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 
 --
 -- AUTO_INCREMENT de la tabla `galeria`
@@ -446,7 +390,7 @@ ALTER TABLE `galeria`
 -- AUTO_INCREMENT de la tabla `patrocinio`
 --
 ALTER TABLE `patrocinio`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT de la tabla `pregunta`
@@ -458,13 +402,13 @@ ALTER TABLE `pregunta`
 -- AUTO_INCREMENT de la tabla `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de la tabla `voto`
 --
 ALTER TABLE `voto`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
 
 --
 -- Restricciones para tablas volcadas
